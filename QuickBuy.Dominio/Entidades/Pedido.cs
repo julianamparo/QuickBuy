29d4c53e20a612de3d415cf4cc0975b074891827
julianamparo <juliana.amparo90@gmail.com>
2020-07-31 01:37:07 -0300

    Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,33 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento  FormaPagamento { get; set; }
+        //Deve ter pelo menos um item ou muitos
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
